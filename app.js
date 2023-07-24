d6f89178af2cf1d81014a886e3d8147154ec7679
Baby Medusa <duarte.baltazar@medusaprotocol.com>
2023-07-24 15:32:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,48 +14,25 @@ initializeFirebase();
 // Create a new Telegraf instance with your Telegram Bot Token
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
+// Command handlers
+bot.start(startCommand);
+bot.command('create', createCommand);
+bot.command('submit', (ctx) => {
+  ctx.reply('You have used /submit command.');
+});
+bot.command('help', helpCommand);
+bot.command('find', findCommand);
+
 // Set the Telegram bot webhook
 async function setWebhook() {
-  const fetch = await import('node-fetch');
-  const botToken = process.env.TELEGRAM_BOT_TOKEN;
   const webhookURL = process.env.HEROKU_WEBHOOK_URL;
-
-  const apiUrl = `https://api.telegram.org/bot${botToken}/setWebhook?url=${webhookURL}/bot${botToken}`;
-
   try {
-    const response = await fetch.default(apiUrl);
-    const data = await response.json();
-    console.log('Webhook set:', data);
+    await bot.telegram.setWebhook(`${webhookURL}/bot${process.env.TELEGRAM_BOT_TOKEN}`);
+    console.log('Webhook set successfully!');
   } catch (error) {
     console.error('Error setting webhook:', error);
   }
 }
 
-
-const port = process.env.PORT || 3000;
-
-
 // Call the setWebhook function to set up the webhook
 setWebhook();
-
-// Command handlers
-bot.start(startCommand);
-
-// /create command handler
-bot.command('create', createCommand);
-
-// /submit command handler
-bot.command('submit', (ctx) => {
-  ctx.reply('You have used /submit command.');
-});
-
-// /help command handler
-bot.command('help', helpCommand);
-
-// /find command handler
-bot.command('find', findCommand);
-
-// Start the bot
-bot.launch();
-
-
