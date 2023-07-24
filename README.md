148cbe4afedeaa7781138b3bc801aa9096ab349a
Baby Medusa <duarte.baltazar@medusaprotocol.com>
2023-07-24 05:08:05 +0100

    Changes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # MemeContestBot
+
 A Web3 Telegram Bot to create meme competitions and earn with submitted memes
+
+To set it up on your own
+Build an .env file with your:
+
+TELEGRAM_BOT_TOKEN=YOUR_TELEGRAMAPI
+
+MIT License
+
+Copyright (c) 2023 Duball69
--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const { createCommand } = require('./commands/create');
 const { submitCommand } = require('./commands/submit');
 const { helpCommand } = require('./commands/help');
 const { findCommand } = require('./commands/find');
-require('dotenv').config();
 
+require('dotenv').config();
 
 // Initialize Firebase
 initializeFirebase();
@@ -14,12 +14,31 @@ initializeFirebase();
 // Create a new Telegraf instance with your Telegram Bot Token
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
+// Set the Telegram bot webhook
+async function setWebhook() {
+  const fetch = await import('node-fetch');
+  const botToken = process.env.TELEGRAM_BOT_TOKEN;
+  const webhookURL = process.env.HEROKU_WEBHOOK_URL;
+
+  const apiUrl = `https://api.telegram.org/bot${botToken}/setWebhook?url=${webhookURL}/bot${botToken}`;
+
+  try {
+    const response = await fetch.default(apiUrl);
+    const data = await response.json();
+    console.log('Webhook set:', data);
+  } catch (error) {
+    console.error('Error setting webhook:', error);
+  }
+}
+
+// Call the setWebhook function to set up the webhook
+setWebhook();
 
 // Command handlers
 bot.start(startCommand);
 
 // /create command handler
-bot.command('create', createCommand); // Add the createCommand function to the command handlers 
+bot.command('create', createCommand);
 
 // /submit command handler
 bot.command('submit', (ctx) => {
@@ -34,4 +53,3 @@ bot.command('find', findCommand);
 
 // Start the bot
 bot.launch();
-
