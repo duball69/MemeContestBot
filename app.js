7fc6ab6fa2165dfa210b56588be8bfe5779971d2
Baby Medusa <duarte.baltazar@medusaprotocol.com>
2023-07-24 06:39:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -53,3 +53,8 @@ bot.command('find', findCommand);
 
 // Start the bot
 bot.launch();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
